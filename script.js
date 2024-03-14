26cfe4f9b22d49fad5f459bfc05c7c1a1ecd068a
Fantine <fantine.rudent@hotmail.fr>
2024-03-14 09:44:16 +0100

    best branch ever

--- script.js
+++ script.js
@@ -1,133 +1,114 @@
-
-const slider = document.querySelector('.carouselList');
+const slider = document.querySelector(".carouselList");
 let isDown = false;
 let startX;
 let scrollLeft;
-const leftBTN = document.querySelector('.buttonLeft');
-const rightBTN = document.querySelector('.buttonRight');
+const leftBTN = document.querySelector(".buttonLeft");
+const rightBTN = document.querySelector(".buttonRight");
 
+// slider.addEventListener("mousedown", (e) => {
+// 	isDown = true;
+// 	slider.classList.add("active");
+// 	startX = e.pageX - slider.offsetLeft;
+// 	scrollLeft = slider.scrollLeft;
+// });
+// slider.addEventListener("mouseleave", (_) => {
+// 	isDown = false;
+// 	slider.classList.remove("active");
+// });
+// slider.addEventListener("mouseup", (_) => {
+// 	isDown = false;
+// 	slider.classList.remove("active");
+// });
+// slider.addEventListener("mousemove", (e) => {
+// 	if (!isDown) return;
+// 	e.preventDefault();
+// 	const x = e.pageX - slider.offsetLeft;
+// 	const SCROLL_SPEED = 10;
+// 	const walk = (x - startX) * SCROLL_SPEED;
+// 	slider.scrollLeft = scrollLeft - walk;
+// });
 
-slider.addEventListener('mousedown', e => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', _ => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', _ => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', e => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const SCROLL_SPEED = 10;
-    const walk = (x - startX) * SCROLL_SPEED;
-    slider.scrollLeft = scrollLeft - walk;
-})
+// leftBTN.addEventListener("click", (e) => {
+// 	isDown = true;
+// 	slider.classList.add("active");
+// 	startX = e.pageX - slider.offsetLeft;
+// 	scrollLeft = slider.scrollLeft;
+// });
 
-
-leftBTN.addEventListener('click', e => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-
-
-function pressRightArrow() {
-    // Simuler l'événement de la touche flèche de droite
-    let event = new KeyboardEvent('keydown', {
-      key: 'ArrowRight',
-      keyCode: 39,
-      which: 39,
-      code: 'ArrowRight'
-    });
-    document.dispatchEvent(event);
-rightBTN.addEventListener('click', pressRightArrow)
-};
+// function pressRightArrow() {
+// 	// Simuler l'événement de la touche flèche de droite
+// 	let event = new KeyboardEvent("keydown", {
+// 		key: "ArrowRight",
+// 		keyCode: 39,
+// 		which: 39,
+// 		code: "ArrowRight",
+// 	});
+// 	document.dispatchEvent(event);
+// 	rightBTN.addEventListener("click", pressRightArrow);
+// }
 
 //  leftBTN.addEventListener(
 
-   
 // });
 
 // rightBTN.addEventListener('click', e => {
-    
+
 // })
 
-function filterPays () {
-    let filtre, tableau, ligne, cellule, i, texte 
+function filterPays() {
+	let filtre, tableau, ligne, cellule, i, texte;
 
-    filtre = document.getElementById("rechercheTourneePays").value.toUpperCase();
-    tableau = document.getElementById("tableTournee");
-    ligne = tableau.getElementsByTagName ("tr")
+	filtre = document
+		.getElementById("rechercheTourneePays")
+		.value.toUpperCase();
+	tableau = document.getElementById("tableTournee");
+	ligne = tableau.getElementsByTagName("tr");
 
-    for (i=0 ; i<ligne.length ; i++)
+	for (i = 0; i < ligne.length; i++) {
+		cellule = ligne[i].getElementsByTagName("td")[1];
+		if (cellule) {
+			texte = cellule.innerText;
+			if (texte.toUpperCase().indexOf(filtre) > -1) {
+				ligne[i].style.display = "";
+			} else {
+				ligne[i].style.display = "none";
+			}
+		}
+	}
 
-    {
-        cellule = ligne [i].getElementsByTagName ("td") [1] ;
-         ;
-        if (cellule) {
-            texte = cellule.innerText;
-            if (texte.toUpperCase().indexOf(filtre)> -1) 
-            { 
-                ligne [i].style.display= "";
-            }
-            else  { 
-                ligne [i].style.display= "none";
-            }
-        }
-    }
-
-    for (i=0 ; i<ligne.length ; i++)
-
-    {
-        cellule = ligne [i].getElementsByTagName ("td") [2] ;
-         ;
-        if (cellule) {
-            texte = cellule.innerText;
-            if (texte.toUpperCase().indexOf(filtre)> -1) 
-            { 
-                ligne [i].style.display= "";
-            }
-            else  { 
-                ligne [i].style.display= "none";
-            }
-        }
-    }
-
-} 
-function filterCity () {
-    let filtre, tableau, ligne, cellule, i, texte 
-
-    filtre = document.getElementById("rechercheTourneeVille").value.toUpperCase();
-    tableau = document.getElementById("tableTournee");
-    ligne = tableau.getElementsByTagName ("tr")
-
-    for (i=0 ; i<ligne.length ; i++)
-
-    {
-        cellule = ligne [i].getElementsByTagName ("td") [1] ;
-        if (cellule) {
-            texte = cellule.innerText;
-            if (texte.toUpperCase().indexOf(filtre)> -1) 
-            { 
-                ligne [i].style.display= "";
-            }
-            else  { 
-                ligne [i].style.display= "none";
-            }
-        }
-    }
+	for (i = 0; i < ligne.length; i++) {
+		cellule = ligne[i].getElementsByTagName("td")[2];
+		if (cellule) {
+			texte = cellule.innerText;
+			if (texte.toUpperCase().indexOf(filtre) > -1) {
+				ligne[i].style.display = "";
+			} else {
+				ligne[i].style.display = "none";
+			}
+		}
+	}
 }
+function filterCity() {
+	let filtre, tableau, ligne, cellule, i, texte;
 
+	filtre = document
+		.getElementById("rechercheTourneeVille")
+		.value.toUpperCase();
+	tableau = document.getElementById("tableTournee");
+	ligne = tableau.getElementsByTagName("tr");
 
-
+	for (i = 0; i < ligne.length; i++) {
+		cellule = ligne[i].getElementsByTagName("td")[1];
+		if (cellule) {
+			texte = cellule.innerText;
+			if (texte.toUpperCase().indexOf(filtre) > -1) {
+				ligne[i].style.display = "";
+			} else {
+				ligne[i].style.display = "none";
+			}
+		}
+	}
+}
 
 /* changement de couleur au survol 
 const title = document.querySelector(".titleTournee");
@@ -141,5 +122,10 @@ title.addEventListener('mouseout', function () {
     title.style.color = "yellow"; // Réinitialisez la couleur lorsqu'on quitte la zone du titre
 });*/
 
-
-
+const titleFantine = document.getElementById("so-cool");
+console.log(titleFantine);
+titleFantine.addEventListener("click", function () {
+	alert(
+		"Fantine is the very best and we are very lucky to have her, we should treat her like she deserves."
+	);
+});
